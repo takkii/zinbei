e43851c14d3decaa2c5948a1cff8da037f1755a8
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-01-18 18:13:57 +0900

    Update.

--- lib/zinbei/timestamp.rb
+++ lib/zinbei/timestamp.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+lib = File.expand_path('exe', __dir__)
+$LOAD_PATH.unshift(exe) unless $LOAD_PATH.include?(exe)
+
 Encoding.default_internal = 'UTF-8'
 
 class Golan
 	def exefile
 		begin
-			exec("./kaminarigumo.exe")
+			exec("kaminarigumo.exe")
 		rescue Exception => e
 			puts e.backtrace
 		end
