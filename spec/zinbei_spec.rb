823ad1e992645e6c6f91048dfa79eb5f0d7315fd
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-03-21 11:51:25 +0900

    Update, rspec.

--- spec/zinbei_spec.rb
+++ spec/zinbei_spec.rb
@@ -13,22 +13,24 @@ RSpec.describe 'Zinbei' do
     require 'himekuri'
   end
 
-  describe 'prop/version' do
-    str = '1.5.3'
-    subject { Zinbei::VERSION }
-    it { is_expected.to eq(str) }
-  end
+  describe 'Zinbei function' do
+    context 'prop/version' do
+      str = '1.5.3'
+      subject { Zinbei::VERSION }
+      it { is_expected.to eq(str) }
+    end
 
-  context 'himekuri' do
-    nen = %w[令和]
-    td = Date.today
-    rei = (nen[0] + "#{(td.year - 2018)}年" + "#{td.month}月" + "#{td.day}日")
-    subject { Object.new.reiwa }
-    it { is_expected.to eq(rei) }
-  end
+    context 'himekuri' do
+      nen = %w[令和]
+      td = Date.today
+      rei = (nen[0] + "#{(td.year - 2018)}年" + "#{td.month}月" + "#{td.day}日")
+      subject { Object.new.reiwa }
+      it { is_expected.to eq(rei) }
+    end
 
-  after do
-    GC.start
+    after do
+      GC.start
+    end
   end
 end
 
