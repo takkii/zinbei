c3528a5aa0353cd310a27bc6f097dab9f0808fa9
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-07-25 15:11:56 +0900

    Update.

--- lib/zinbei/version.rb
+++ lib/zinbei/version.rb
@@ -3,14 +3,14 @@
 require 'date'
 
 begin
-  # zinbei_version = x.x.x-xxxx.xx.xx
-  module Zinbei
-    @zinbei_version = '3.0.0'
-    # t = Date.today
-    # build_day = t.strftime('%Y.%m.%d')
-    # VERSION = "#{zinbei_version}-#{build_day}".freeze
-    VERSION = @zinbei_version.freeze
-  end
+# zinbei_version = x.x.x-xxxx.xx.xx
+module Zinbei
+  zinbei_version = '3.0.1'
+  t = Date.today
+  build_day = t.strftime('%Y.%m.%d')
+  VERSION = "#{zinbei_version}-#{build_day}".freeze
+  #VERSION = "#{zinbei_version}"
+end
 rescue StandardError => e
   puts e.backtrace
 ensure
